d4ddbe8f0934198ae0122a009e5bd8c183f1034a
Nikita Chebotarevskiy <mikita.chebotarevskiy@gmail.com>
2023-02-01 20:14:04 +0300

    add db and admin panel for polls

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.site_header = 'PollsApp Administration'
+admin.site.site_title = 'PollsApp Administration'
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner', 'date_publish', 'last_modified', 'is_active')
+    ordering = ('-last_modified', 'title')
+    list_display_links = ('id', 'title')
+    list_editable = ('is_active',)
+    list_filter = ('owner', 'date_publish', 'is_active')
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'choice_text', 'poll')
+
+
+class VoteAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Vote, VoteAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Title")
+    topic = models.TextField(verbose_name="Topic of the poll")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_publish = models.DateTimeField(auto_now_add=True, verbose_name="Date of publishing")
+    last_modified = models.DateTimeField(auto_now=True, verbose_name="Last modified")
+    is_active = models.BooleanField(default=True, verbose_name="Is still active")
+
+    def __str__(self):
+        return self.title
+
+
+class Choice(models.Model):
+    choice_text = models.TextField(verbose_name="Text")
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} selected {self.choice} in {self.poll} "
